aa2f1901a029075a248c18479d7ba9380c932dfb
AsliHan61 <asli.61.y@gmail.com>
2023-04-16 13:05:53 +0200

    VikingsLab

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,50 @@
 // Soldier
-class Soldier {}
-
+class Soldier {
+    constructor(health, strength){
+        this.health = health;
+        this.strength = strength;
+}
+attack(){
+    return this.strength;
+}
+receiveDamage(damage){
+    this.health -= damage;
+}
+}
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(nameValue, helathValue, strengthValue) {
+        super(helathValue, strengthValue)
+        this.name = nameValue
+    }
+}
 
 // Saxon
 class Saxon {}
+class Saxon extends Soldier {
+    receiveDamage(damage) {
+        console.dir(this)
+        this.health -= damage
+        if (this.health === 0) {
+            return `A Saxon has died in combat`
+        } else {
+            return `A Saxon has receives ${damage} points of damage`
+        }
+    }
+}
 
 // War
-class War {}
+class War {
+    constructor() {
+        this.vikingArmy = []
+        this.saxonArmy = []
+    }
+    addVikings(Viking) {
+        this.vikingArmy.push(Viking)
+    }
+    addSaxon(Saxon){
+        this.saxonArmy.push(Saxon)
+    }
+    vikingAttack(){}
+}
+
